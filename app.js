e7770cc7f788e47edd4b6bbe44907993ed9adc19
dogukan-sys <karademird67@gmail.com>
2023-09-03 19:07:53 +0200

    refactor looping functionality of makemove

--- app.js
+++ app.js
@@ -19,52 +19,55 @@ const gameboard = (() => {
     [1,5,9]
 ];
 
-
   // To make Player selections
   const makeMove = (function () {
-  const cells = document.querySelectorAll(".cell");
-  cells.forEach(cell => {
-    cell.addEventListener("click", (e) => {
-      if (player1.turn == true && winner == null && e.target.textContent == '') {
-        cell.textContent = player1.mark
-        player1.board.push(parseInt(e.target.id))
-        player1.turn = false
-        player2.turn = true
-      } else if (player2.turn == true && winner == null && e.target.textContent == '') {
-        cell.textContent = player2.mark
-        player2.board.push(parseInt(e.target.id))
-        player1.turn = true
-        player2.turn = false
-      } else {return}
-      gameTurn++
-      gameCheck()
-    })
-  });
-  })();
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+      cell.addEventListener("click", (e) => {
+        if (player1.turn == true && winner == null && e.target.textContent == '') {
+          cell.textContent = player1.mark
+          player1.board.push(parseInt(e.target.id))
+          player1.turn = false
+          player2.turn = true
+        } else if (player2.turn == true && winner == null && e.target.textContent == '') {
+          cell.textContent = player2.mark
+          player2.board.push(parseInt(e.target.id))
+          player1.turn = true
+          player2.turn = false
+        }
+        gameTurn++
+        console.log(gameTurn)
+        gameCheck()
+      })
+    });
 
-
-  const gameCheck = () => {
-    for (const combo of winCombos) {
-      if (combo.every(num => player1.board.includes(num))) {
-          // Player 1 has a winning combo
-          winner = player1;
-          return;
-      } else if (combo.every(num => player2.board.includes(num))) {
-          // Player 2 has a winning combo
-          winner = player2;
-          return;
-      } else if (gameTurn == 9) {
-          winner = 'Tie'
+    gameCheck = () => {
+      for (const combo of winCombos) {
+        if (combo.every(num => player1.board.includes(num))) {
+            // Player 1 has a winning combo
+            winner = player1;
+            gameReset()
+        } else if (combo.every(num => player2.board.includes(num))) {
+            // Player 2 has a winning combo
+            winner = player2;
+            gameReset()
+        } else if (gameTurn == 9) {
+            winner = 'Tie'
+            gameReset()
+        }
       }
     }
-  }
 
-  const gameReset = () => {
-    player1.board = []
-    player2.board = []
-    player1.turn = true
-    player2.turn = false
-    let gameTurn = 0
-    let winner= null
-  }
+    gameReset = () => {
+      player1.board = []
+      player2.board = []
+      player1.turn = true
+      player2.turn = false
+      gameTurn = 0
+      winner = null
+      cells.forEach(cell => {
+        cell.textContent = ''
+      })
+    }
+  })();
 })();
